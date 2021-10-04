dc632caac22ce92a6fab68c39a012d2acf3e9de8
Vishvesh Patel <patelvishvesh85@gmail.com>
2021-10-04 17:30:29 +0530

    added

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, url_for, redirect
-import pymysql.cursors
+# import pymysql.cursors
 app = Flask(__name__)
 
-Con = pymysql.Connect(host="127.0.0.1", port=3307, user="", passwd="",db='')
+# Con = pymysql.Connect(host="127.0.0.1", port=3307, user="", passwd="",db='')
 
-cur = Con.cursor()
+# cur = Con.cursor()
 app.secret_key = "abdhghsb"
 
 @app.route("/")
@@ -22,39 +22,39 @@ def services():
 
 @app.route("/career", methods=["GET", "POST"])
 def career():
-    if request.method == "POST":
-        firstname = request.form["firstname"]
-        lastname = request.form["lastname"]
-        email = request.form["email"]
-        contact = request.form["contact"]
-        position = request.form["position"]
-        experience = request.form["experience"]
-        date = request.form["date"]
+    # if request.method == "POST":
+    #     firstname = request.form["firstname"]
+    #     lastname = request.form["lastname"]
+    #     email = request.form["email"]
+    #     contact = request.form["contact"]
+    #     position = request.form["position"]
+    #     experience = request.form["experience"]
+    #     date = request.form["date"]
 
-        sql = "INSERT INTO career (firstname,lastname,email,contact,position,experience,date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = (firstname, lastname,email,contact,position, experience,date)
-        cur.execute(sql,val)
-        Con.commit()
+    #     sql = "INSERT INTO career (firstname,lastname,email,contact,position,experience,date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    #     val = (firstname, lastname,email,contact,position, experience,date)
+    #     cur.execute(sql,val)
+    #     Con.commit()
 
     #     return redirect(url_for('career'))
     return render_template("career.html")
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-    if request.method == "POST":
-        name = request.form['name']
-        subject = request.form['Subject']
-        email = request.form['email']
-        phone = request.form['phone']
-        address = request.form['address']
-        call = request.form['call']
-        message = request.form['message']
+    # if request.method == "POST":
+    #     name = request.form['name']
+    #     subject = request.form['Subject']
+    #     email = request.form['email']
+    #     phone = request.form['phone']
+    #     address = request.form['address']
+    #     call = request.form['call']
+    #     message = request.form['message']
 
-        sql = "INSERT INTO contactdetails (name, email, contact,subject,address,time,message) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = (name,email,phone,subject,address,call,message)
-        cur.execute(sql,val)
-        Con.commit()
-        return redirect(url_for('contact'))
+    #     sql = "INSERT INTO contactdetails (name, email, contact,subject,address,time,message) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    #     val = (name,email,phone,subject,address,call,message)
+    #     cur.execute(sql,val)
+    #     Con.commit()
+    #     return redirect(url_for('contact'))
     return render_template("contact.html")
 
 @app.route("/ongoing-projects")
